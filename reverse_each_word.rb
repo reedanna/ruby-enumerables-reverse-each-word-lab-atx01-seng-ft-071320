2f67e42a2b4edf6b9ce61f22ae65e6fb62bab57d
Anna Nicole Reed <reedanna@mail.com>
2020-06-29 23:21:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(string)
   string_array = string.split(/ /)
-  reversed_array = []
   reversed_string = ""
   string_array.collect do |item|
-    reversed = item.reverse
-    reversed_array.push(reversed)
+    item.reverse
   end
   reversed_string = reversed_array.join(" ")
 end
